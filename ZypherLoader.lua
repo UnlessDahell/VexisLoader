53d1faab8442c1794788f1c05161399206e7e6fa
UnlessDahell <148602200+UnlessDahell@users.noreply.github.com>
2025-03-20 14:27:39 +0700

    ZypherLoader.lua

--- ZypherLoader.lua
+++ ZypherLoader.lua
@@ -1,52 +1,82 @@
 if not game:IsLoaded() then
-   game.Loaded:Wait()
+game.Loaded:Wait()
 end
 
 local success, Rayfield = pcall(function()
-   return loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+return loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 end)
 
 if not success then
-   warn("Failed to load Rayfield UI. Please check the URL or try again later.")
-   return
+warn("Failed to load Rayfield UI. Please check the URL or try again later.")
+return
 end
 
 local Window = Rayfield:CreateWindow({
-   Name = "Zypher Script Loader (.gg/wDj3ve2K)",
-   Icon = 82284779245358, 
-   LoadingTitle = "Wait until GUI setup",
-   LoadingSubtitle = "by Sir.Zypher and Skider",
-   Theme = "Serenity",
+Name = "Zypher Script Loader (.gg/wDj3ve2K)",
+Icon = 82284779245358,
+LoadingTitle = "Wait until GUI setup",
+LoadingSubtitle = "by Sir.Zypher and Skider",
+Theme = "Serenity",
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false,
+DisableRayfieldPrompts = false,
+DisableBuildWarnings = false,
 
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = "Zypher",  
-      FileName = "Zypher"
-   },
+ConfigurationSaving = {
+Enabled = true,
+FolderName = true,
+FileName = "Zypher"
+},
 
-   Discord = {
-      Enabled = false, 
-      Invite = ".gg/wDj3ve2K", 
-      RememberJoins = true 
-   },
+Discord = {
+Enabled = false,
+Invite = ".gg/wDj3ve2K",
+RememberJoins = true
+},
 
-   KeySystem = true, 
-   KeySettings = {
-      Title = "Zypher Hub Key system",
-      Subtitle = "Zypher Hub Need More Member! (.gg/wDj3ve2K)",
-      Note = "Join Our Discord for News And Key!! .gg/wDj3ve2K", 
-      FileName = "ZTeam", 
-      SaveKey = true,  -- This should allow saving the key
-      GrabKeyFromSite = false, 
-      Key = {"Zypher-227-945"} 
-   }
+KeySystem = true,
+KeySettings = {
+Title = "Zypher Hub Key system",
+Subtitle = "Zypher Hub Need More Member! (.gg/wDj3ve2K)",
+Note = "Join Our Discord for News And Key!! .gg/wDj3ve2K",
+FileName = "ZTeam",
+SaveKey = true,
+GrabKeyFromSite = false,
+Key = {"Zypher-227-945"}
+}
 })
 
-Rayfield.KeySystem.OnKeyAccepted = function(enteredKey)
-   if Window.ConfigurationSaving.Enabled and Window.KeySettings.SaveKey then
-      Rayfield:SaveConfiguration()
-   end
-end
+local MainTab = Window:CreateTab("Zypher Loader", 132272873219669)
+local MainSection = MainTab:CreateSection("Main Loader")
+
+local Button = MainTab:CreateButton({
+Name = "Fearise Hub : Blue lock Rivals :",
+Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UnlessDahell/FeariseHub/refs/heads/main/Fearise_Hub_Bluelock_newloader.lua", true))()
+end,
+})
+
+Rayfield:Notify({
+Title = "Zypher Notifying",
+Content = "Please don't forget to join our community server!\n(https://discord.gg/wDj3ve2K)",
+Duration = 25,
+Image = 4483362458,
+})
+
+local CommunitySection = MainTab:CreateSection("Our Discord Community Server")
+
+local Button2 = MainTab:CreateButton({
+Name = "Discord Link Click to Get Here!",
+Callback = function()
+setclipboard("https://discord.gg/PNuYYAtd")
+end,
+})
+
+local DestroyTab = Window:CreateTab("Destroy This Loader Ui", 130985545137273)
+local DestroySection = DestroyTab:CreateSection("Destroy This Loader")
+
+local Button = DestroyTab:CreateButton({
+Name = "Destroy Loader",
+Callback = function()
+Rayfield:Destroy()
+end,
+})
