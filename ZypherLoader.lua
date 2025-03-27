9130207725fe31d73d09afec43f48579268d807b
UnlessDahell <148602200+UnlessDahell@users.noreply.github.com>
2025-03-27 14:20:30 +0700

    ZypherLoader.lua

--- ZypherLoader.lua
+++ ZypherLoader.lua
@@ -1,4 +1,3 @@
-
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
@@ -74,51 +73,51 @@ end
 local MainTab = Window:CreateTab("Zypher Loader", 132272873219669)
 local MainSection = MainTab:CreateSection("Main Loader")
 
-local Button = MainTab:CreateButton({
-Name = "Fearise Hub : Blue lock Rivals :",
-Callback = function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UnlessDahell/FeariseHub/refs/heads/main/Fearise_Hub_Bluelock_newloader.lua", true))()
-end,
+local Button1 = MainTab:CreateButton({
+    Name = "Fearise Hub : Blue lock Rivals :",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/UnlessDahell/FeariseHub/refs/heads/main/Fearise_Hub_Bluelock_newloader.lua", true))()
+    end,
 })
 
 local Button2 = MainTab:CreateButton({
-Name = "Zypher Hub : Forsaken (Preview Ended) :",
-Callback = function()
-print("Preview ended now it's time to continue full version of Zypher Forsaken")
-end,
+    Name = "Zypher Hub : Forsaken (Preview Ended) :",
+    Callback = function()
+        print("Preview ended now it's time to continue full version of Zypher Forsaken")
+    end,
 })
 
 local FearisePreUPDSection = MainTab:CreateSection("This is PreUPDATE version before Update main one (Test Version)")
 
 local Button3 = MainTab:CreateButton({
-Name = "Fearise Hub PreUPD v3.2 (Test Version) :",
-Callback = function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/UnlessDahell/FeariseHub/refs/heads/main/PreUPD-FeariseHubV3.2.lua",true))()
-end,
+    Name = "Fearise Hub PreUPD v3.2 (Test Version) :",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/UnlessDahell/FeariseHub/refs/heads/main/PreUPD-FeariseHubV3.2.lua",true))()
+    end,
 })
 
 Rayfield:Notify({
-Title = "Zypher Notifying",
-Content = "Please don't forget to join our community server!! (https://discord.gg/p5ynKu5f)",
-Duration = 25,
-Image = 4483362458,
+    Title = "Zypher Notifying",
+    Content = "Please don't forget to join our community server!! (https://discord.gg/p5ynKu5f)",
+    Duration = 25,
+    Image = 4483362458,
 })
 
 local CommunitySection = MainTab:CreateSection("Our Discord Community Server")
 
-local Button2 = MainTab:CreateButton({
-Name = "Discord Link Click to Get Here!",
-Callback = function()
-setclipboard("https://discord.gg/aTNg2Dcw")
-end,
+local Button4 = MainTab:CreateButton({
+    Name = "Discord Link Click to Get Here!",
+    Callback = function()
+        setclipboard("https://discord.gg/aTNg2Dcw")
+    end,
 })
 
 local DestroyTab = Window:CreateTab("Destroy This Loader Ui", 130985545137273)
 local DestroySection = DestroyTab:CreateSection("Destroy This Loader")
 
-local Button = DestroyTab:CreateButton({
-Name = "Destroy Loader",
-Callback = function()
-Rayfield:Destroy()
-end,
+local DestroyButton = DestroyTab:CreateButton({
+    Name = "Destroy Loader",
+    Callback = function()
+        Rayfield:Destroy()
+    end,
 })
